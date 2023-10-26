6598864eccaca1a421de0d80df855483340b3872
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-10-26 21:28:12 +0500

    Interfaces Created

--- AdoNetProject/Interfaces/IEmployeeRepository.cs
+++ AdoNetProject/Interfaces/IEmployeeRepository.cs
@@ -6,7 +6,7 @@ namespace AdoNetProject.Interfaces
     public interface IEmployeeRepository
     {
         void CreateEmployee(EmployeeDTO employee);
-        void UpdateEmployee(EmployeeDTO employee);
+        void UpdateEmployee(int EmployeeId,EmployeeDTO employee);
         void DeleteEmployee(int EmployeeId);
         void EmployeeDeepDelete(int EmployeeId);
         void GetAllEmployees();
